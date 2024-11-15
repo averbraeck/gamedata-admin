dc98571e5f13f39b46ae166a65647c36b6f5530c
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-11-15 22:57:58 +0100

    Do not print popup text on screen when there is an error

--- src/main/java/nl/gamedata/admin/ModalWindowUtils.java
+++ src/main/java/nl/gamedata/admin/ModalWindowUtils.java
@@ -6,7 +6,7 @@ public class ModalWindowUtils
     public static void popup(AdminData data, String title, String message, String okMethod)
     {
         // make popup
-        System.err.println("popup with title [" + title + "] and message: " + message);
+        // System.err.println("popup with title [" + title + "] and message: " + message);
         StringBuilder s = new StringBuilder();
         s.append("<p>");
         s.append(message);
