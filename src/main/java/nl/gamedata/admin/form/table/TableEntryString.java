e9cee5d08e179859b3e6c76091cc2700819ba161
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-12-14 23:50:45 +0100

    Small mistake in String html

--- src/main/java/nl/gamedata/admin/form/table/TableEntryString.java
+++ src/main/java/nl/gamedata/admin/form/table/TableEntryString.java
@@ -89,7 +89,6 @@ public class TableEntryString extends AbstractTableEntry<TableEntryString, Strin
             s.append("&nbsp;&nbsp;<input type=\"checkbox\" name=\"");
             s.append(getTableField().getName() + "-null\" value=\"null\"");
             s.append(getLastEnteredValue() == null ? " checked" : "");
-            s.append(" />");
             if (isReadOnly() || !getForm().isEdit())
                 s.append(" readonly />");
             else
