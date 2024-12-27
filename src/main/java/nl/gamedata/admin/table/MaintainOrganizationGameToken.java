0705601525fe3f3ba586c1b25aa4e4b99a94f405
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-12-27 19:43:35 +0100

    First implementation of MaintainOrganizationGameToken

---
+++ src/main/java/nl/gamedata/admin/table/MaintainOrganizationGameToken.java
@@ -0,0 +1,81 @@
+package nl.gamedata.admin.table;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import nl.gamedata.admin.AdminData;
+import nl.gamedata.admin.AdminTable;
+import nl.gamedata.admin.form.table.TableEntryBoolean;
+import nl.gamedata.admin.form.table.TableEntryPickRecord;
+import nl.gamedata.admin.form.table.TableEntryString;
+import nl.gamedata.admin.form.table.TableForm;
+import nl.gamedata.common.Access;
+import nl.gamedata.common.SqlUtils;
+import nl.gamedata.data.Tables;
+import nl.gamedata.data.tables.records.OrganizationGameTokenRecord;
+
+/**
+ * MaintainOrganizationGame takes care of the game access screen.
+ * <p>
+ * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://github.com/averbraeck/gamedata-admin/LICENSE">GameData project License</a>.
+ * </p>
+ * @author <a href="https://github.com/averbraeck">Alexander Verbraeck</a>
+ */
+public class MaintainOrganizationGameToken
+{
+    public static void table(final AdminData data, final HttpServletRequest request, final String menuChoice)
+    {
+        AdminTable table = new AdminTable(data, "Game Access Tokens", "Org-Game");
+        boolean access = data.isSuperAdmin() || data.isOrganizationAdmin() || data.hasOrganizationGameAccess(Access.EDIT);
+        table.setNewButton(access);
+        table.setHeader("Org-Game", "Name", "Value", "Reader", "Writer");
+        DSLContext dslContext = DSL.using(data.getDataSource(), SQLDialect.MYSQL);
+        List<Record> ogtList = dslContext.selectFrom(Tables.ORGANIZATION_GAME_TOKEN.join(Tables.ORGANIZATION_GAME)
+                .on(Tables.ORGANIZATION_GAME_TOKEN.ORGANIZATION_GAME_ID.eq(Tables.ORGANIZATION_GAME.ID))).fetch();
+        for (var ogt : ogtList)
+        {
+            for (Integer ogId : data.getOrganizationGameAccess().keySet())
+            {
+                if (ogId.equals(ogt.getValue(Tables.ORGANIZATION_GAME.ID)))
+                {
+                    int id = ogt.getValue(Tables.ORGANIZATION_GAME_TOKEN.ID);
+                    String orgGame = ogt.getValue(Tables.ORGANIZATION_GAME.NAME);
+                    String name = ogt.getValue(Tables.ORGANIZATION_GAME_TOKEN.NAME);
+                    String value = ogt.getValue(Tables.ORGANIZATION_GAME_TOKEN.VALUE);
+                    String writer = ogt.getValue(Tables.ORGANIZATION_GAME_TOKEN.WRITER) == 0 ? "N" : "Y";
+                    String reader = ogt.getValue(Tables.ORGANIZATION_GAME_TOKEN.READER) == 0 ? "N" : "Y";
+                    // TODO? String org = ogt.getValue(Tables.ORGANIZATION.CODE);
+                    // TODO? String game = ogt.getValue(Tables.GAME.CODE);
+                    table.addRow(id, false, access, access, orgGame, name, value, writer, reader);
+                    break;
+                }
+            }
+        }
+        table.process();
+    }
+
+    public static void edit(final AdminData data, final HttpServletRequest request, final String click, final int recordId)
+    {
+        OrganizationGameTokenRecord ogt = recordId == 0 ? Tables.ORGANIZATION_GAME_TOKEN.newRecord()
+                : SqlUtils.readRecordFromId(data, Tables.ORGANIZATION_GAME_TOKEN, recordId);
+        data.setEditRecord(ogt);
+        TableForm form = new TableForm(data);
+        form.startForm();
+        form.setHeader("Game Access Token", click, recordId);
+        form.addEntry(new TableEntryPickRecord(Tables.ORGANIZATION_GAME_TOKEN.ORGANIZATION_GAME_ID, ogt).setPickTable(data,
+                data.getOrganizationGamePicklist(Access.EDIT), Tables.ORGANIZATION_GAME.ID, Tables.ORGANIZATION_GAME.NAME));
+        form.addEntry(new TableEntryString(Tables.ORGANIZATION_GAME_TOKEN.NAME, ogt));
+        form.addEntry(new TableEntryString(Tables.ORGANIZATION_GAME_TOKEN.VALUE, ogt));
+        form.addEntry(new TableEntryBoolean(Tables.ORGANIZATION_GAME_TOKEN.WRITER, ogt));
+        form.addEntry(new TableEntryBoolean(Tables.ORGANIZATION_GAME_TOKEN.READER, ogt));
+        form.endForm();
+        data.setContent(form.process());
+    }
+}
